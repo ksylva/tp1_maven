bd0656a651847cb5eddc023e981574b14b1a124b
lekaiser <sylvanus010@gmail.com>
2021-01-06 17:59:06 +0100

    Failed to execute goal org.apache.maven.plugins:maven-site-plugin:3.7.1:site (default-site) on project tp1_maven: Error generating maven-javadoc-plugin:3.2.0:aggregate-no-fork report

---
+++ src/main/java/fr/istic/m1/App.java
@@ -0,0 +1,17 @@
+package fr.istic.m1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * Main method. Entry point of application
+     * @param args array of strings
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
